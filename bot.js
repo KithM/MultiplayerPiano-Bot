1ba11d45b51a452e3937ca8a593e5917df4ab2c7
KithM <malachite.akin@gmail.com>
2018-05-13 16:16:33 -0500

    Update bot.js with copyright and usage info

---
+++ bot.js
@@ -0,0 +1,6 @@
+// Copyright (C) Kith M and all collaborators of this project
+// All Rights Reserved
+
+// USAGE
+// The following may be entered into the console on
+// http://www.multiplayerpiano.com
