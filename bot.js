20898c86f59faf85d8e87153c0f24056d5b883f6
KarlMarx1269287 <39247382+KarlMarx1269287@users.noreply.github.com>
2018-05-14 14:15:08 -0700

    Bot.js K edit 3
    Added chat art for moders

--- bot.js
+++ bot.js
@@ -12,7 +12,7 @@ MPP.client.on("a" Function(msg) {
   });
   //it's easyier to have higher ups in all below classes
   //this allows the script to run faster
-  //it only has to check one arry
+  //it only has to check one array
   Owners=['9c42aae508d2b93a6036a7c7',
          '5d0b7aad380a5fc463b62570'] // creaters
   Admins=['9c42aae508d2b93a6036a7c7',
@@ -872,6 +872,395 @@ MPP.client.on("a", function(msg) {
         }
     }
 });
+ MPP.client.on('participant added', function(part) {
+    if(Aclass.includes(part._id)) {
+        MPP.chat.send(part.name+''+WelcomeArray[Math.floor(Math.random()*WelcomeArray.length)]+' Lets welcome them.');
+    }
+});
+ MPP.client.on('a', function (msg) {
+    var isModers = (Moders.indexOf(msg.p._id) !==-1);  
+    var cmd = msg.a.split(' ')[0].toLowerCase();
+    var input = msg.a.substring(cmd.length).trim();
+    if (cmd == '/lennylist') {
+        if(isModers) {
+            MPP.chat.send('Lennys: /lenny, /guns, /bear2, /army, /hugs, /magic, /sneak, /table, /table2, /hearteyes, /trump, /butterfly, /wink, /blush, /smile, /smile2, /smile3, /smile4, /dance');
+        } else {
+            MPP.chat.send('You\'re not an Admin. To learn more type /admin');}
+    }
+});
+MPP.client.on('a', function (msg) {
+    var isModers = (Moders.indexOf(msg.p._id) !==-1);  
+    var cmd = msg.a.split(' ')[0].toLowerCase();
+    var input = msg.a.substring(cmd.length).trim();
+    if (cmd == '/lenny') {
+        if(isModers) {
+            MPP.chat.send('( ͡° ͜ʖ ͡°)');
+
+        } else {
+            MPP.chat.send('You\'re not an Admin. To learn more type /admin');}
+    }
+});
+MPP.client.on('a', function (msg) {
+    var isModers = (Moders.indexOf(msg.p._id) !==-1);  
+    var cmd = msg.a.split(' ')[0].toLowerCase();
+    var input = msg.a.substring(cmd.length).trim();
+    if (cmd == '/guns') {
+        if(isModers) {
+            MPP.chat.send('̿̿ ̿̿ ̿̿ ̿\'̿\'\̵͇̿̿\з= ( ▀ ͜͞ʖ▀) =ε/̵͇̿̿/’̿’̿ ̿ ̿̿ ̿̿ ̿̿');
+
+        } else {
+            MPP.chat.send('You\'re not an Admin. To learn more type /admin');}
+    }
+});
+MPP.client.on('a', function (msg) {
+    var isModers = (Moders.indexOf(msg.p._id) !==-1);  
+    var cmd = msg.a.split(' ')[0].toLowerCase();
+    var input = msg.a.substring(cmd.length).trim();
+    if (cmd == '/bear2') {
+        if(isModers) {
+            MPP.chat.send('ʕ•ᴥ•ʔ');
+
+        } else {
+            MPP.chat.send('You\'re not an Admin. To learn more type /admin');}
+    }
+});
+MPP.client.on('a', function (msg) {
+    var isModers = (Moders.indexOf(msg.p._id) !==-1);  
+    var cmd = msg.a.split(' ')[0].toLowerCase();
+    var input = msg.a.substring(cmd.length).trim();
+    if (cmd == '/army') {
+        if(isModers) {
+            MPP.chat.send('( ͡°( ͡° ͜ʖ( ͡° ͜ʖ ͡°)ʖ ͡°) ͡°)');
+
+        } else {
+            MPP.chat.send('You\'re not an Admin. To learn more type /admin');}
+    }
+});
+MPP.client.on('a', function (msg) {
+    var isModers = (Moders.indexOf(msg.p._id) !==-1);  
+    var cmd = msg.a.split(' ')[0].toLowerCase();
+    var input = msg.a.substring(cmd.length).trim();
+    if (cmd == '/hug2') {
+        if(isModers) {
+            MPP.chat.send('(づ｡◕‿‿◕｡)づ');
+
+        } else {
+            MPP.chat.send('You\'re not an Admin. To learn more type /admin');}
+    }
+});
+MPP.client.on('a', function (msg) {
+    var isModers = (Moders.indexOf(msg.p._id) !==-1);  
+    var cmd = msg.a.split(' ')[0].toLowerCase();
+    var input = msg.a.substring(cmd.length).trim();
+    if (cmd == '/magic') {
+        if(isModers) {
+            MPP.chat.send('(ﾉ◕ヮ◕)ﾉ*:･ﾟ✧ ✧ﾟ･: *ヽ(◕ヮ◕ヽ)');
+
+        } else {
+            MPP.chat.send('You\'re not an Admin. To learn more type /admin');}
+    }
+});
+MPP.client.on('a', function (msg) {
+    var isModers = (Moders.indexOf(msg.p._id) !==-1);  
+    var cmd = msg.a.split(' ')[0].toLowerCase();
+    var input = msg.a.substring(cmd.length).trim();
+    if (cmd == '/sneak') {
+        if(isModers) {
+            MPP.chat.send('┬┴┬┴┤ ͜ʖ ͡°) ├┬┴┬┴');
+
+        } else {
+            MPP.chat.send('You\'re not an Admin. To learn more type /admin');}
+    }
+});
+MPP.client.on('a', function (msg) {
+    var isModers = (Moders.indexOf(msg.p._id) !==-1);  
+    var cmd = msg.a.split(' ')[0].toLowerCase();
+    var input = msg.a.substring(cmd.length).trim();
+    if (cmd == '/table') {
+        if(isModers) {
+            MPP.chat.send('(ノಠ益ಠ)ノ彡┻━┻');
+
+        } else {
+            MPP.chat.send('You\'re not an Admin. To learn more type /admin');}
+    }
+});
+MPP.client.on('a', function (msg) {
+    var isModers = (Moders.indexOf(msg.p._id) !==-1);  
+    var cmd = msg.a.split(' ')[0].toLowerCase();
+    var input = msg.a.substring(cmd.length).trim();
+    if (cmd == '/hearteyes') {
+        if(isModers) {
+            MPP.chat.send('♥‿♥');
+
+        } else {
+            MPP.chat.send('You\'re not an Admin. To learn more type /admin');}
+    }
+});
+MPP.client.on('a', function (msg) {
+    var isModers = (Moders.indexOf(msg.p._id) !==-1);  
+    var cmd = msg.a.split(' ')[0].toLowerCase();
+    var input = msg.a.substring(cmd.length).trim();
+    if (cmd == '/table2') {
+        if(isModers) {
+            MPP.chat.send('┻━┻ ︵ヽ(`Д´)ﾉ︵ ┻━┻');
+
+        } else {
+            MPP.chat.send('You\'re not an Admin. To learn more type /admin');}
+    }
+});
+MPP.client.on('a', function (msg) {
+    var isModers = (Moders.indexOf(msg.p._id) !==-1);  
+    var cmd = msg.a.split(' ')[0].toLowerCase();
+    var input = msg.a.substring(cmd.length).trim();
+    if (cmd == '/smile') {
+        if(isModers) {
+            MPP.chat.send('｡◕‿‿◕｡');
+
+        } else {
+            MPP.chat.send('You\'re not an Admin. To learn more type /admin');}
+    }
+});
+MPP.client.on('a', function (msg) {
+    var isModers = (Moders.indexOf(msg.p._id) !==-1);  
+    var cmd = msg.a.split(' ')[0].toLowerCase();
+    var input = msg.a.substring(cmd.length).trim();
+    if (cmd == '/smile2') {
+        if(isModers) {
+            MPP.chat.send('｡◕‿◕｡');
+
+        } else {
+            MPP.chat.send('You\'re not an Admin. To learn more type /admin');}
+    }
+});MPP.client.on('a', function (msg) {
+    var isModers = (Moders.indexOf(msg.p._id) !==-1);  
+    var cmd = msg.a.split(' ')[0].toLowerCase();
+    var input = msg.a.substring(cmd.length).trim();
+    if (cmd == '/trump') {
+        if(isModers) {
+            MPP.chat.send('ლ,ᔑ•ﺪ͟͠•ᔐ.ლ');
+
+        } else {
+            MPP.chat.send('You\'re not an Admin. To learn more type /admin');}
+    }
+});
+MPP.client.on('a', function (msg) {
+    var isModers = (Moders.indexOf(msg.p._id) !==-1);  
+    var cmd = msg.a.split(' ')[0].toLowerCase();
+    var input = msg.a.substring(cmd.length).trim();
+    if (cmd == '/butterfly') {
+        if(isModers) {
+            MPP.chat.send('Ƹ̵̡Ӝ̵̨̄Ʒ');
+
+        } else {
+            MPP.chat.send('You\'re not an Admin. To learn more type /admin');}
+    }
+});
+MPP.client.on('a', function (msg) {
+    var isModers = (Moders.indexOf(msg.p._id) !==-1);  
+    var cmd = msg.a.split(' ')[0].toLowerCase();
+    var input = msg.a.substring(cmd.length).trim();
+    if (cmd == '/wink') {
+        if(isModers) {
+            MPP.chat.send('ಠ‿↼');
+
+        } else {
+            MPP.chat.send('You\'re not an Admin. To learn more type /admin');}
+    }
+});
+MPP.client.on('a', function (msg) {
+    var isModers = (Moders.indexOf(msg.p._id) !==-1);  
+    var cmd = msg.a.split(' ')[0].toLowerCase();
+    var input = msg.a.substring(cmd.length).trim();
+    if (cmd == '/blush') {
+        if(isModers) {
+            MPP.chat.send('(▰˘◡˘▰)');
+
+        } else {
+            MPP.chat.send('You\'re not an Admin. To learn more type /admin');}
+    }
+});
+MPP.client.on('a', function (msg) {
+    var isModers = (Moders.indexOf(msg.p._id) !==-1);  
+    var cmd = msg.a.split(' ')[0].toLowerCase();
+    var input = msg.a.substring(cmd.length).trim();
+    if (cmd == '/smile3') {
+        if(isModers) {
+            MPP.chat.send('^̮^');
+
+        } else {
+            MPP.chat.send('You\'re not an Admin. To learn more type /admin');}
+    }
+});
+MPP.client.on('a', function (msg) {
+    var isModers = (Moders.indexOf(msg.p._id) !==-1);  
+    var cmd = msg.a.split(' ')[0].toLowerCase();
+    var input = msg.a.substring(cmd.length).trim();
+    if (cmd == '/smile4') {
+        if(isModers) {
+            MPP.chat.send('(ᵔᴥᵔ)');
+
+        } else {
+            MPP.chat.send('You\'re not an Admin. To learn more type /admin');}
+    }
+});
+MPP.client.on('a', function (msg) {
+    var isModers = (Moders.indexOf(msg.p._id) !==-1);  
+    var cmd = msg.a.split(' ')[0].toLowerCase();
+    var input = msg.a.substring(cmd.length).trim();
+    if (cmd == '/dance') {
+        if(isModers) {
+            MPP.chat.send('(づ￣ ³￣)づ');
+
+        } else {
+            MPP.chat.send('You\'re not an Admin. To learn more type /admin');}
+    }
+});
+MPP.client.on('a', function (msg) {
+    var isModers = (Moders.indexOf(msg.p._id) !==-1);  
+    var cmd = msg.a.split(' ')[0].toLowerCase();
+    var input = msg.a.substring(cmd.length).trim();
+    if (cmd == '/sayart') {
+        if(isModers) {
+            MPP.chat.send('\u00D7,.\u00B7\u00B4\u00A8\'\u00B0\u00F7\u00B7..\u00A7 '+ input +' \u00A7.\u00B7\u00B4\u00A8\'\u00B0\u00F7\u00B7..\u00D7');
+        } else {
+            MPP.chat.send('You\'re not an Admin. To learn more type /admin ');}
+    }
+});
+MPP.client.on('a', function (msg) {
+    var isModers = (Moders.indexOf(msg.p._id) !==-1);  
+    var cmd = msg.a.split(' ')[0].toLowerCase();
+    var input = msg.a.substring(cmd.length).trim();
+    if (cmd ==  '/bear') {
+        if(isModers) {
+            MPP.chat.send('\u2500\u2500\u2500\u2584\u2580\u2580\u2580\u2584\u2584\u2584\u2584\u2584\u2584\u2584\u2580\u2580\u2580\u2584\u2500\u2500\u2500');
+            MPP.chat.send('\u2500\u2500\u2500\u2588\u2592\u2592\u2591\u2591\u2591\u2591\u2591\u2591\u2591\u2591\u2591\u2592\u2592\u2588\u2500\u2500\u2500');
+            MPP.chat.send('\u2500\u2500\u2500\u2500\u2588\u2591\u2591\u2588\u2591\u2591\u2591\u2591\u2591\u2588\u2591\u2591\u2588\u2500\u2500\u2500\u2500');
+            MPP.chat.send('\u2500\u2584\u2584\u2500\u2500\u2588\u2591\u2591\u2591\u2580\u2588\u2580\u2591\u2591\u2591\u2588\u2500\u2500\u2584\u2584\u2500');
+            MPP.chat.send('\u2588\u2591\u2591\u2588\u2500\u2580\u2584\u2591\u2591\u2591\u2591\u2591\u2591\u2591\u2584\u2580\u2500\u2588\u2591\u2591\u2588');
+        } else {
+            MPP.chat.send('You\'re not an Admin. To learn more type /admin ');}
+    }
+});
+MPP.client.on('a', function (msg) {
+
+    var isModers = (Moders.indexOf(msg.p._id) !==-1);  
+    var cmd = msg.a.split(' ')[0].toLowerCase();
+    var input = msg.a.substring(cmd.length).trim();
+    if (cmd ==  '/tank') {
+        if(isModers) {
+            MPP.chat.send('\u2591\u2591\u2591\u2591\u2591\u2591\u2588\u2588\u2588\u2588\u2588\u2588\u2588 ]\u2584\u2584\u2584\u2584\u2584\u2584\u2584\u2584\u2583 ');
+            MPP.chat.send('\u2582\u2584\u2585\u2588\u2588\u2588\u2588\u2588\u2588\u2588\u2588\u2588\u2585\u2584\u2583\u2582 ');
+            MPP.chat.send('I\u2588\u2588\u2588\u2588\u2588\u2588\u2588\u2588\u2588\u2588\u2588\u2588\u2588\u2588\u2588\u2588\u2588\u2588\u2588].');
+            MPP.chat.send('\u25E5\u2299\u25B2\u2299\u25B2\u2299\u25B2\u2299\u25B2\u2299\u25B2\u2299\u25B2\u2299\u25E4 ');
+        } else {
+            MPP.chat.send('You\'re not an Admin. To learn more type /admin ');}
+    }
+});
+MPP.client.on('a', function (msg) {
+    var isModers = (Moders.indexOf(msg.p._id) !==-1);  
+    var cmd = msg.a.split(' ')[0].toLowerCase();
+    var input = msg.a.substring(cmd.length).trim();
+    if (cmd ==  '/sunglasses') {
+        if(isModers) {
+            MPP.chat.send('\u2584\u2584\u2584\u2584\u2584\u2584\u2584\u2584\u2584\u2584\u2584\u2584\u2584\u2584\u2584\u2584\u2584\u2584\u2584\u2584\u2584\u2584\u2584\u2584\u2584\u2584\u2584\u2584\u2584\u2584\u2584\u2584\u2584\u2584\u2584');
+            MPP.chat.send('\u2588\u2591\u2591\u2591\u2591\u2591\u2591\u2591\u2591\u2580\u2588\u2584\u2580\u2584\u2580\u2588\u2588\u2588\u2588\u2588\u2588\u2591\u2580\u2588\u2584\u2580\u2584\u2580\u2588\u2588\u2588\u2588\u2588\u2588  ');
+            MPP.chat.send('\u2591\u2591\u2591\u2591 \u2591\u2591\u2591\u2591\u2591\u2591\u2591\u2580\u2588\u2584\u2588\u2584\u2588\u2588\u2588\u2580\u2591\u2591\u2591 \u2580\u2588\u2584\u2588\u2584\u2588\u2588\u2588');
+        } else {
+            MPP.chat.send('You\'re not an Admin. To learn more type /admin ');}
+    }
+});
+MPP.client.on('a', function (msg) {
+    var isModers = (Moders.indexOf(msg.p._id) !==-1);  
+    var cmd = msg.a.split(' ')[0].toLowerCase();
+    var input = msg.a.substring(cmd.length).trim();
+    if (cmd ==  '/hateme') {
+        if(isModers) {
+            MPP.chat.send('\u2554\u2557');
+            MPP.chat.send('\u2551\u255A\u2566\u2550\u2563\u255A\u2566\u2550\u2557\u2554\u2550\u2550\u2566\u2550\u2557');
+            MPP.chat.send('\u2551\u2551\u2551\u256C\u2551\u2554\u2563\u2569\u2563\u2551\u2551\u2551\u2551\u2569\u2563');
+            MPP.chat.send('\u255A\u2569\u2569\u2569\u2569\u2550\u2569\u2550\u255D\u255A\u2569\u2569\u2569\u2550\u255D');
+        } else {
+            MPP.chat.send('You\'re not an Admin. To learn more type /admin ');}
+    }
+});
+MPP.client.on('a', function (msg) {
+    var isModers = (Moders.indexOf(msg.p._id) !==-1);  
+    var cmd = msg.a.split(' ')[0].toLowerCase();
+    var input = msg.a.substring(cmd.length).trim();
+    if (cmd ==  '/piano') {
+        if(isModers) {
+            MPP.chat.send('\u2551\u2591\u2588\u2591\u2588\u2591\u2551\u2591\u2588\u2591\u2588\u2591\u2588\u2591\u2551\u2591\u2588\u2591\u2588\u2591\u2551');
+            MPP.chat.send('\u2551\u2591\u2588\u2591\u2588\u2591\u2551\u2591\u2588\u2591\u2588\u2591\u2588\u2591\u2551\u2591\u2588\u2591\u2588\u2591\u2551');
+            MPP.chat.send('\u2551\u2591\u2551\u2591\u2551\u2591\u2551\u2591\u2551\u2591\u2551\u2591\u2551\u2591\u2551\u2591\u2551\u2591\u2551\u2591\u2551');
+        } else {
+            MPP.chat.send('You\'re not an Admin. To learn more type /admin ');}
+    }
+});
+
+MPP.client.on('a', function (msg) {
+    var isModers = (Moders.indexOf(msg.p._id) !==-1);  
+    var cmd = msg.a.split(' ')[0].toLowerCase();
+    var input = msg.a.substring(cmd.length).trim();
+    if (cmd == '/artlist') {
+        if(isModers) {
+            MPP.chat.send('Type for text art: /piano, /hateme, /sunglasses, /tank, /bear, /cat, /gun, /music, /lennylist');
+            MPP.chat.send('Type for text art, input need: /love, /sayart');
+        } else {
+            MPP.chat.send('You\'re not an Admin. To learn more type /admin');}
+    }
+});
+MPP.client.on('a', function (msg) {
+    var isModers = (Moders.indexOf(msg.p._id) !==-1);  
+    var cmd = msg.a.split(' ')[0].toLowerCase();
+    var input = msg.a.substring(cmd.length).trim();
+    if (cmd ==  '/cat') {
+        if(isModers) {
+            MPP.chat.send('\u2500\u2500\u2500\u2500\u2500\u2500\u2584\u2580\u2584\u2500\u2500\u2500\u2500\u2500\u2584\u2580\u2584');
+            MPP.chat.send('\u2500\u2500\u2500\u2500\u2500\u2584\u2588\u2591\u2591\u2580\u2580\u2580\u2580\u2580\u2591\u2591\u2588\u2584');
+            MPP.chat.send('\u2500\u2584\u2584\u2500\u2500\u2588\u2591\u2591\u2591\u2591\u2591\u2591\u2591\u2591\u2591\u2591\u2591\u2588\u2500\u2500\u2584\u2584');
+            MPP.chat.send('\u2588\u2584\u2584\u2588\u2500\u2588\u2591\u2591\u2580\u2591\u2591\u252C\u2591\u2591\u2580\u2591\u2591\u2588\u2500\u2588\u2584\u2584\u2588');
+        } else {
+            MPP.chat.send('You\'re not an Admin. To learn more type /admin');}
+    }
+});
+MPP.client.on('a', function (msg) {
+    var isModers = (Moders.indexOf(msg.p._id) !==-1);  
+    var cmd = msg.a.split(' ')[0].toLowerCase();
+    var input = msg.a.substring(cmd.length).trim();
+    if (cmd ==  '/love') {
+        if(isModers) {
+            MPP.chat.send('\u2554\u2550\u2550\u2557');
+            MPP.chat.send('\u255A\u2557\u2554\u255D');
+            MPP.chat.send('\u2554\u255D(\u00AF`v\u00B4\u00AF)');
+            MPP.chat.send('\u255A\u2550\u2550`.\u00B8.['+ input +']');
+        } else {
+            MPP.chat.send('You\'re not an Admin. To learn more type /admin');}
+    }
+});
+MPP.client.on('a', function (msg) {
+    var isModers = (Moders.indexOf(msg.p._id) !==-1);
+    var cmd = msg.a.split(' ')[0].toLowerCase();
+    var input = msg.a.substring(cmd.length).trim();
+    if (cmd ==  '/gun') {
+        if(isModers) {
+            MPP.chat.send('\u2591\u2584\u258C\u2591\u2591\u2591\u2591\u2591\u2591\u2591\u2591\u2591\u2584');
+            MPP.chat.send('\u2588\u2588\u2588\u2588\u2588\u2588\u2588\u2588\u2588\u2588\u2588\u2588\u2584');
+            MPP.chat.send('\u2591\u2591\u2591\u2591\u2591\u2591\u2591\u2591\u2580\u2590\u2588\u2588\u2588\u2588');
+            MPP.chat.send('\u2591\u2591\u2591\u2591\u2591\u2591\u2591\u2591\u2591\u2591\u2591\u2590\u2588\u2588\u258C');
+        } else {
+            MPP.chat.send('You\'re not an Admin. To learn more type /admin');}
+    }
+});
+WelcomeArray=[' walks in with a big smile. Unfortunately, their pants are unzipped.',
+' flies through the door, destroying the thriving weed farm. This is the third time this week...',
+' joins the club. Time to party!',
+', the manager of this establishment, walks in swiftly with their briefcase.',
+' enters the building. Everyone, act natural. ',
+' smashes through a window on a out-of-control hose. ',
+' waltz into the room. Get it waltz, because ... oh forget it.',
+' bursts thought the wall dressed as the Kool-aid man. "OH YEAH!!!"',
+', you\'re late, get to work, slave.'
+]
  kills=['a screw',' a bow','a magnet','a nail file','coasters','deodorant','headphones','pants','a chair','a hanger','an ipod','an outlet','lip gloss','soap','a hair brush','a credit card','a blanket','chalk','perfume','a puddle','a paint brush','nail clippers','twister™','a slipper','a USB drive',' a pair of glasses','a computer','a cell phone','a box','some clothes','a lamp shade','a buckel','eye linertable','a washing machine','a zipper','teddies','a door','a bed','video gamessocks',' street lights','a key chain',' tooth picks','an eraser','a sketch pad','a keyboard','a sandal','a piano','a clay pot']
 knifes=['butterfly knife',' long sword','dagger','Boning Knife','Bread Knife','Butcher Knife','Breaking Knife','Chef\'s Knife','Cimeter','Cleaver']
 sreason=['the FBI was after them',' they were sad',' gave a bad hand shake',' forgot to flush','"It\'s the  Best way to make sure I\'m not dead already."','"They\'re waiting on me down below to start the party."','"The cost of living rises, but death remains affordable."']
